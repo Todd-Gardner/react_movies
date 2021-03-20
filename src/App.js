0b9c5f664bdeb4f72cb9684eb7dd10c20fb5b58b
Todd-Gardner <Todd-Gardner@users.noreply.github.com>
2021-03-20 14:33:45 -0400

    Added movie component and styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
+import React, { useState, useEffect } from "react";
+import Movie from "./components/Movie";
+
+// get API key from local .env
+const { REACT_APP_TMDB_API_KEY } = process.env;
+
+// list of tmdb API urls
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?sort=popularity.desc&api_key=${REACT_APP_TMDB_API_KEY}&page=1`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?&api_key=${REACT_APP_TMDB_API_KEY}&query=`;
+// const IMAGE_API = `https://image.tmdb.org/t/p/w1280`;
+// const TRAILER = ``;
 
 function App() {
-  return <div>Hello Movies</div>;
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function getFeaturedMovies() {
+      const response = await fetch(FEATURED_API);
+      const jsonResponse = await response.json();
+      setMovies(jsonResponse.results);
+    }
+    getFeaturedMovies();
+  }, []);
+
+  return (
+    <div className="movieContainer">
+      {movies /*.length > 0*/ &&
+        movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Movie.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const IMAGE_API = `https://image.tmdb.org/t/p/w1280`;
+
+const Movie = ({ title, poster_path, overview, vote_average }) => (
+  <div className="movie">
+    <img src={IMAGE_API + poster_path} alt={title} />
+    <div className="movieInfo">
+      <h3>{title}</h3>
+      <span>{vote_average}</span>
+    </div>
+  </div>
+);
+
+export default Movie;
