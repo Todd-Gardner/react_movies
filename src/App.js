4d11d236bff6fdd8d75ad49fadfe3c2381b1269a
Todd-Gardner <Todd-Gardner@users.noreply.github.com>
2021-03-23 15:36:01 -0400

    Adding search functionallity. Trying 'auto' search after 3 char entered

--- src/App.js
+++ src/App.js
@@ -12,20 +12,64 @@ const SEARCH_API = `https://api.themoviedb.org/3/search/movie?&api_key=${REACT_A
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
 
+  // get initial list of movies on loading
   useEffect(() => {
-    async function getFeaturedMovies() {
-      const response = await fetch(FEATURED_API);
-      const jsonResponse = await response.json();
-      setMovies(jsonResponse.results);
-    }
-    getFeaturedMovies();
+    getMovies(FEATURED_API);
   }, []);
 
+  // search for movies when search input changes
+  useEffect(() => {
+    if (searchInput.length > 3) {
+      getMovies(SEARCH_API + searchInput);
+    }
+  }, [searchInput]);
+
+  async function getMovies(API) {
+    const response = await fetch(API);
+    const jsonData = await response.json();
+    setMovies(jsonData.results);
+  }
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchInput) {
+      getMovies(SEARCH_API + searchInput);
+      setSearchInput("");
+    }
+  };
+
+  // const onChangeHandler = (e) => {
+  //   setSearchInput(e.target.value);
+  //   if (e.target.value.length > 3) {
+  //     getMovies(SEARCH_API + searchInput);
+  //     // for using promise, not async/await
+  //     // fetch(SEARCH_API + searchInput)
+  //     //   .then((res) => res.json())
+  //     //   .then((data) => setMovies(data.results));
+  //   }
+  // };
+
   return (
-    <div className="movieContainer">
-      {movies /*.length > 0*/ &&
-        movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+    <div>
+      {/* change header to Search component */}
+      <header>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            className="searchBar"
+            placeholder="Search movies..."
+            value={searchInput}
+            // onChange={onChangeHandler}
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
+        </form>
+      </header>
+      <div className="movieContainer">
+        {movies /*.length > 0*/ &&
+          movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+      </div>
     </div>
   );
 }
