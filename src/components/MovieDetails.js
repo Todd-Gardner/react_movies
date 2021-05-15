1b434849044c9b0c98bb690ffa9930adc733f2d2
Todd-Gardner <Todd-Gardner@users.noreply.github.com>
2021-05-15 12:23:08 -0400

    Turned MovieDetails into a modal

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -42,7 +42,7 @@ const MovieDetails = ({
       const videoResults = videos.results;
 
       const video = videoResults.find((video, index) => {
-        if (video.site === "YouTube" && video.type === "Trailer") {
+        if (video.site === "YouTube"){//&& video.type === "Trailer") {
           return true;
         } else {
           return false;
@@ -57,62 +57,64 @@ const MovieDetails = ({
   }, [credits, videos]);
 
   return (
-    <div className="detailsContainer">
-      <div className="detailsHeader">
-        <h2>
-          {title}({releaseDate.substring(0, 4)})
-        </h2>
-        <button onClick={closeModal}>X</button>
-      </div>
-      <div className="movieDetails">
-        <div className="movieDescription">{overview}</div>
-        {videoKey ? (
-          <VideoPreview videoKey={videoKey} />
-        ) : (
-          <div className="moviePoster">
-            <img
-              src={
-                { poster_path }
-                  ? IMAGE_API + poster_path
-                  : "https://images.unsplash.com/photo-1533488765986-dfa2a9939acd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80"
-              }
-              alt={title}
-            />
-          </div>
-        )}
-      </div>
-      <div className="actorInfoContainer">
-        {actorList &&
-          actorList.map((actor) => {
-            return (
-              <div className="actorInfo" key={actor.id}>
-                {/* {actor.character}
+    <div className="modal">
+      <div className="detailsContainer">
+        <div className="detailsHeader">
+          <h2>
+            {title}({releaseDate.substring(0, 4)})
+          </h2>
+          <button onClick={closeModal}>X</button>
+        </div>
+        <div className="movieDetails">
+          <div className="movieDescription">{overview}</div>
+          {videoKey ? (
+            <VideoPreview videoKey={videoKey} />
+          ) : (
+            <div className="moviePoster">
+              <img
+                src={
+                  { poster_path }
+                    ? IMAGE_API + poster_path
+                    : "https://images.unsplash.com/photo-1533488765986-dfa2a9939acd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80"
+                }
+                alt={title}
+              />
+            </div>
+          )}
+        </div>
+        <div className="actorInfoContainer">
+          {actorList &&
+            actorList.map((actor) => {
+              return (
+                <div className="actorInfo" key={actor.id}>
+                  {/* {actor.character}
                 <br />
                 Played by: */}
-                {actor.name}
-                <img
-                  src={
-                    actor.profilePic
-                      ? IMAGE_API + actor.profilePic
-                      : "https://images.unsplash.com/photo-1616582607004-eba71ce01e07?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=639&q=80"
-                  }
-                  alt="profile"
-                />
-              </div>
-            );
-          })}
+                  {actor.name}
+                  <img
+                    src={
+                      actor.profilePic
+                        ? IMAGE_API + actor.profilePic
+                        : "https://images.unsplash.com/photo-1616582607004-eba71ce01e07?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=639&q=80"
+                    }
+                    alt="profile"
+                  />
+                </div>
+              );
+            })}
+        </div>
+        <button
+          style={{
+            width: "100%",
+            margin: 5,
+            color: "white",
+            background: "#373b69",
+          }}
+          onClick={closeModal}
+        >
+          Close
+        </button>
       </div>
-      <button
-        style={{
-          width: "100%",
-          margin: 5,
-          color: "white",
-          background: "#373b69",
-        }}
-        onClick={closeModal}
-      >
-        Close
-      </button>
     </div>
   );
 };
